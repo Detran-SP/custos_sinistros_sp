66dddbe8d4ce157e516dfbf0581dd4396ab93433
pabsantos <pabsantos21@gmail.com>
2025-05-12 15:58:23 -0300

    atualiza a tabela de custos (#4)

--- R/catalogo_custos.R
+++ R/catalogo_custos.R
@@ -1,4 +1,21 @@
-create_custos_pessoas = function(tp_sinistros, fator_ipca) {
+load_ipca <- function(path) {
+    path_ipca = path
+    df_ipca_raw = read_excel(
+        path_ipca, 
+        range = "A440:C451", 
+        col_names = c("ano", "mes", "valor_ipca")
+    )
+
+    df_ipca_raw |> 
+        mutate(
+            ano = "2024",
+            mes = seq(1, 12),
+            date = ym(paste0(ano, "-", mes))
+        )
+}
+
+
+create_custos_pessoas = function(tp_sinistros, df_ipca, ipca_antigo) {
     tipo_vitimas = c("Ileso", "Leve", "Grave", "Fatal")
     expand_grid(tipo_vitimas, tp_sinistros) |> 
         mutate(
@@ -9,11 +26,14 @@ create_custos_pessoas = function(tp_sinistros, fator_ipca) {
                 22421.06, 125133.91, 141155.96, # Grave
                 199.28, 335172.20, 433286.69    # Fatal
             ),
-            custos_atual = custos * fator_ipca
-        )
+            ipca_antigo = ipca_antigo
+        ) |> 
+        expand_grid(df_ipca |> select(date, valor_ipca)) |> 
+        mutate(custos_atual = valor_ipca / ipca_antigo * custos) |> 
+        select(tipo_vitimas, tp_sinistros, date, custos_atual)
 }
 
-create_custos_veiculos <- function(tp_sinistros, fator_ipca) {
+create_custos_veiculos <- function(tp_sinistros, df_ipca, ipca_antigo) {
     tipo_veiculos = c(
         "Automóvel", "Motocicleta", "Bicicleta", "Caminhão", "Ônibus", "Outros"
     )
@@ -28,23 +48,32 @@ create_custos_veiculos <- function(tp_sinistros, fator_ipca) {
                 16069.30, 10536.86, 20686.60,  # Ônibus
                 10307.36, 80108.63, 81209.29   # Outros
             ),
-            custos_atual = custos * fator_ipca
-        )
+            ipca_antigo = ipca_antigo
+        ) |> 
+        expand_grid(df_ipca |> select(date, valor_ipca)) |> 
+        mutate(custos_atual = valor_ipca / ipca_antigo * custos) |> 
+        select(tipo_veiculos, tp_sinistros, date, custos_atual)
 }
 
-create_custos_inst <- function(tp_sinistros, fator_ipca) {
+create_custos_inst <- function(tp_sinistros, df_ipca, ipca_antigo) {
     tibble(
         tipo_sinistro = tp_sinistros,
         # Extraídos de IPEA (2020)
-        custos = c(453.35, 338.33, 653.06),
-        custos_atual = custos * fator_ipca
-    )
+        custos = c(453.35, 338.33, 653.06)
+    ) |> 
+        mutate(ipca_antigo = ipca_antigo) |> 
+        expand_grid(df_ipca |> select(date, valor_ipca)) |> 
+        mutate(custos_atual = valor_ipca / ipca_antigo * custos) |> 
+        select(tipo_sinistro, date, custos_atual)
 }
 
-create_custos_urbanos <- function(tp_sinistros, fator_ipca) {
+create_custos_urbanos <- function(tp_sinistros, df_ipca, ipca_antigo) {
     tibble(
         tipo_sinistro = tp_sinistros,
-        custos = c(3261.54, 17459.69, 144477.50),
-        custos_atual = custos * fator_ipca
-    )
+        custos = c(3261.54, 17459.69, 144477.50)
+    ) |> 
+        mutate(ipca_antigo = ipca_antigo) |> 
+        expand_grid(df_ipca |> select(date, valor_ipca)) |> 
+        mutate(custos_atual = valor_ipca / ipca_antigo * custos) |> 
+        select(tipo_sinistro, date, custos_atual)
 }
